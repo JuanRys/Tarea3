8d9e01ccf3eeb2673256c9b06e29c8d23d4ffcfe
JuanRys <112053118+JuanRys@users.noreply.github.com>
2022-08-25 12:42:18 -0500

    Add files via upload

---
+++ tarea 3.py
@@ -0,0 +1,14 @@
+# -*- coding: iso-8859-15 -*-
+# Este codigo ha sido generado por el modulo psexport 20180802-w32 de PSeInt.
+# Es posible que el codigo generado no sea completamente correcto. Si encuentra
+# errores por favor reportelos en el foro (http://pseint.sourceforge.net).
+
+
+if __name__ == '__main__':
+	print "Hoy puedo cenar Tacos U Pizza"
+	tacos = raw_input()
+	if tacos>=60:
+		print "La pizza cuesta 90 pesos"
+	else:
+		print "Los tacos cuestan 60 pesos"
+
